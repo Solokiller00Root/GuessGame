1b576303d9f93bb3dbe974c14b90ce124474aba6
unknown <chrismpampas1@gmail.com>
2023-09-06 19:41:10 +0300

    Next js init setup

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Guess Game",
+  description: "Guess game for web dev cody hackathon",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body >{children}</body>
+    </html>
+  );
+}
