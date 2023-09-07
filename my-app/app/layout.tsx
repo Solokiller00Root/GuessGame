de6eb49a7705f6df211036bf8f8396fea9107233
Solokiller00Root <115373706+Solokiller00Root@users.noreply.github.com>
2023-09-07 13:01:03 +0300

    Github log in

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,19 +1,33 @@
 import "./globals.css";
 import type { Metadata } from "next";
 
+
+
+
+
+import SessionProvider from "@/components/SessionProvider";
+import { getServerSession } from "next-auth";
 export const metadata: Metadata = {
   title: "Guess Game",
   description: "Guess game for web dev cody hackathon",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
   return (
     <html lang="en">
-      <body className="bg-background">{children}</body>
+      <body className="bg-background">
+          <SessionProvider session={session}>
+          <main>
+          
+           {children}
+        </main>
+          </SessionProvider>
+        </body>
     </html>
   );
 }
