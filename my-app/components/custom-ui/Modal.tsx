63558bbed93a6bcff6dc0cd535cf255cf024decd
nikagoci <86802785+nikagoci@users.noreply.github.com>
2023-09-10 17:03:15 +0400

    fix form wrap

--- my-app/components/custom-ui/Modal.tsx
+++ my-app/components/custom-ui/Modal.tsx
@@ -17,21 +17,20 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-
 type FormDataType = {
   gameName: string;
   privacy: "private" | "public";
   rounds: number;
-  password: string
-}
+  password: string;
+};
 
 export default function Modal() {
   const [formData, setFormData] = useState<FormDataType>({
     gameName: "",
     privacy: "private",
     rounds: 0,
-    password: ""
-  })
+    password: "",
+  });
   const { data: session } = useSession();
   const router = useRouter();
   const user = useQuery(api.users.getUserByUsername, {
@@ -39,18 +38,17 @@ export default function Modal() {
   });
   const createGame = useMutation(api.games.createGame);
 
-  console.log(user)
   const updateFormData = (field: string, value: string | number) => {
-    setFormData(prev => ({
+    setFormData((prev) => ({
       ...prev,
-      [field]: value
-    }))
-  }
+      [field]: value,
+    }));
+  };
 
-  const submitHandler = async(e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    if(user) {
+    if (user) {
       const gameId = await createGame({
         name: formData.gameName,
         owner: user._id,
@@ -58,11 +56,11 @@ export default function Modal() {
         rounds: formData.rounds,
         status: "waiting",
         players: [user._id],
-        password: formData.password
-      })
+        password: formData.password,
+      });
       router.push(`/play/${gameId}`);
     }
-  }
+  };
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -75,60 +73,72 @@ export default function Modal() {
           <DialogTitle>Ceate Game</DialogTitle>
           <DialogDescription>Choose options to start game</DialogDescription>
         </DialogHeader>
-        <form onSubmit={submitHandler} className="grid gap-4 py-4">
-          <div className="grid items-center grid-cols-4 gap-4">
-            <Label htmlFor="name">Lobby Name</Label>
-            <Input id="name" placeholder="Eagles" className="col-span-3" onChange={(e) => updateFormData("gameName", e.target.value)} />
-          </div>
-          <div className="grid items-center grid-cols-4 gap-4">
-            <Label htmlFor="rounds">Number of rounds</Label>
-            <Input
-              type="number"
-              placeholder="5"
-              id="rounds"
-              className="col-span-3"
-              onChange={(e) => updateFormData("rounds", +e.target.value)}
-            />
-          </div>
-          <div className="grid items-center grid-cols-4 gap-4">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              type="text"
-              placeholder="12345"
-              id="password"
-              className="col-span-3"
-              onChange={(e) => updateFormData("password", e.target.value)}
-            />
-          </div>
-          <div className="grid items-center grid-cols-4 gap-4">
-            <Label htmlFor="private">Privacy</Label>
-            <div onChange={(e: ChangeEvent<HTMLInputElement>) => updateFormData("privacy", e.target.value)} className="flex col-span-3 gap-x-4">
-              <div className="flex items-center gap-x-1">
-                <input
-                  id="private"
-                  type="radio"
-                  value="private"
-                  name="radio"
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 dark:ring-offset-gray-800"
-                />
-                <Label htmlFor="private">Private</Label>
-              </div>
-              <div className="flex items-center gap-x-1">
-                <input
-                  id="public"
-                  type="radio"
-                  value="public"
-                  name="radio"
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 dark:ring-offset-gray-800"
-                />
-                <Label htmlFor="public">Public</Label>
+        <form onSubmit={submitHandler}>
+          <div className="grid gap-4 py-4">
+            <div className="grid items-center grid-cols-4 gap-4">
+              <Label htmlFor="name">Lobby Name</Label>
+              <Input
+                id="name"
+                placeholder="Eagles"
+                className="col-span-3"
+                onChange={(e) => updateFormData("gameName", e.target.value)}
+              />
+            </div>
+            <div className="grid items-center grid-cols-4 gap-4">
+              <Label htmlFor="rounds">Number of rounds</Label>
+              <Input
+                type="number"
+                placeholder="5"
+                id="rounds"
+                className="col-span-3"
+                onChange={(e) => updateFormData("rounds", +e.target.value)}
+              />
+            </div>
+            <div className="grid items-center grid-cols-4 gap-4">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                type="text"
+                placeholder="12345"
+                id="password"
+                className="col-span-3"
+                onChange={(e) => updateFormData("password", e.target.value)}
+              />
+            </div>
+            <div className="grid items-center grid-cols-4 gap-4">
+              <Label htmlFor="private">Privacy</Label>
+              <div
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  updateFormData("privacy", e.target.value)
+                }
+                className="flex col-span-3 gap-x-4"
+              >
+                <div className="flex items-center gap-x-1">
+                  <input
+                    id="private"
+                    type="radio"
+                    value="private"
+                    name="radio"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 dark:ring-offset-gray-800"
+                  />
+                  <Label htmlFor="private">Private</Label>
+                </div>
+                <div className="flex items-center gap-x-1">
+                  <input
+                    id="public"
+                    type="radio"
+                    value="public"
+                    name="radio"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 dark:ring-offset-gray-800"
+                  />
+                  <Label htmlFor="public">Public</Label>
+                </div>
               </div>
             </div>
           </div>
+          <DialogFooter>
+            <Button type="submit">Create Lobby</Button>
+          </DialogFooter>
         </form>
-        <DialogFooter>
-          <Button type="submit">Create Lobby</Button>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
