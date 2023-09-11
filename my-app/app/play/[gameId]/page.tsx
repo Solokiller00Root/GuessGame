3d8d6949988a8df745115689991fa33bf0a3c7f6
N3mEee <n3mebusiness@gmail.com>
2023-09-11 13:30:19 +0200

    Deleted the extra code

--- my-app/app/play/[gameId]/page.tsx
+++ my-app/app/play/[gameId]/page.tsx
@@ -1,23 +1,7 @@
-"use client"
+"use client";
 
 import Game from "@/components/Game";
-import { useMutation, useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
-import { Id } from "@/convex/_generated/dataModel";
-import { redirect, useParams, useRouter } from "next/navigation";
-
-
-export default  function Play() {
-  const params = useParams();
-  const gameId = params.gameId as Id<"games">;
-
-  const game =  useQuery(api.games.getGameById, { gameId });
-
-
-console.log(game)
-console.log(gameId)
-
-
+export default function Play() {
   return (
     <div>
       <Game />
