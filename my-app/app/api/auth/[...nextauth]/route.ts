337d377f18acd553c39da131c7f6e4e9a025d71d
N3mEee <n3mebusiness@gmail.com>
2023-09-11 19:49:09 +0200

    Try fix ("handler" is not a valid Route export field)

--- my-app/app/api/auth/[...nextauth]/route.ts
+++ my-app/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
