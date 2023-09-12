35020d59b6cbf7f6d067cd2072c6110780f43d3f
N3mEee <n3mebusiness@gmail.com>
2023-09-12 18:19:22 +0200

    Try fix route.ts types

--- my-app/app/api/auth/[...nextauth]/route.ts
+++ my-app/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
+
 import GitHubProvider from "next-auth/providers/github";
 
-const authOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
