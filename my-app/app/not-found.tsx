043372243f2e036a60b4c9f39435bcf89a17e30a
Solokiller00Root <115373706+Solokiller00Root@users.noreply.github.com>
2023-09-14 14:03:39 +0300

    fixed error name final

--- my-app/app/not-found.tsx
+++ my-app/app/not-found.tsx
@@ -5,22 +5,24 @@ import lottie from "lottie-web";
 import animationData from "@/public/assets/404.json";
 
 export default function NotFoundPage() {
-  const container = useRef(null);
+  const container = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    lottie.loadAnimation({
-      container: container.current,
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      animationData: animationData,
-    });
+    if (container.current) {
+      lottie.loadAnimation({
+        container: container.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+      });
+    }
   }, []);
 
   return (
     <section className="w-screen h-[75vh] flex justify-center items-center text-white">
       <div ref={container} style={{ width: "300px", height: "300px" }}></div>
-
+      <h1>404</h1>
     </section>
   );
 }
